875d27127b5e2d0419f83975b4b0ff2933260226
Aditya Patel <aditya.nikhil.patel@gmail.com>
2018-05-31 20:34:33 -0400

    added library and sql to create db

---
+++ nfl/create_db.sql
@@ -0,0 +1,28 @@
+use nfl;
+create table games(
+  id int not null,
+  season_id int not null,
+  game_start datetime not null,
+  location varchar(255),
+  home_team_id int not null,
+  away_team_id int not null,
+  score_home tinyint unsigned,
+  score_away tinyint unsigned,
+  primary key(id));
+
+
+create table seasons(
+  id int not null,
+  season_year int not null,
+  season_type varchar(255) not null,
+  superbowl_champ varchar(255)
+);
+
+create table teams(
+  id int not null,
+  name varchar(255) not null,
+  abbr varchar(255) not null,
+  city varchar(255) not null,
+  primary key (id)
+);
+show databases;
---
+++ nfl/library.py
@@ -0,0 +1,13 @@
+
+
+season_ids = {'2014-regular': 1,
+              '2015-playoff': 2,
+              '2015-regular': 3,
+              '2016-playoff': 4,
+              '2016-regular': 5,
+              '2017-playoff': 6,
+              '2017-regular': 7,
+              '2018-playoff': 8,
+              '2018-regular': 9}
+
+
